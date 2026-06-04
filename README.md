198cd5a4a6fb27488a097dd1c7275efa14ec012e
stevalves <ealves1710@hotmail.com>
2026-06-04 02:50:08 -0300

    feat: finalizado colab + preparando streamlit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Detecção de Fraudes em Cartão de Crédito
+
+## Integrantes
+* Estevão Alves dos Santos - 1990000
+* Vanessa Kaori Kurauchi - 2002344
+
+## Projeto (De acordo com o PDF com a descrição do trabalho)
+* Problema: Fraudes causam bilhões em prejuízos anuais. O desafio é identificar transações fraudulentas sem bloquear transações legítimas indevidamente
+* Objetivo: Desenvolver e comparar dados de transações classificação
+* Dataset: Credit Card Fraud Detection (Kaggle)
+> Dataset desbalanceado demais, existindo 284.315 transações legítimas e apenas 492 fraudes (0.17%)
+
+## Metodologia e Tecnologias
+* Técnicas: Pipeline com Scaler, Undersampling e StratifiedKFold, Tuning via RandomizedSearchCV (Já feito na P1)
+* Modelos Testados: Logistic Regression, AdaBoost, Random Forest
+* Modelo Escolhido: Random Forest (Otimizado por AUC-PR e ajuste de Threshold na Validação) -> 85% ACC! Excelente
+* Tecnologias: Python, Scikit-Learn, Pandas, Streamlit
+
+## Resultados e tratativas
+Como estava muito desbalanceado, a acurácia não foi levada muito em consideração pois consideramos AUC-PR e o Recall acima dele. o threshold de 0.85 que foi definido no conjunto da validação, conseguimos maximizar a identificação de ameaças reais (Mesmo as vezes pegando algumas transações legítimas)
+
+## Como Executar Localmente
+1. Instale as dependências: `pip install -r requirements.txt`
+2. Rode o app do Streamlit: `streamlit run app.py`
+
+## Limitações
+Como as variáveis V1 a V28 foram anonimizadas pelo distribuidor dos dados, ficou impossível de criar regras de negócios baseadas no dia-a-dia para facilitar o entendimento
---
+++ app.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import joblib
+
+#- Configuração da Página
+st.set_page_config(page_title="Detecção de Fraudes", page_icon="pesquisa", layout="wide")
+
+st.title("Sistema de Detecção de Fraudes em Cartões")
+st.markdown("Simulador para verificar se uma transação é Legítima ou Fraude.")
+
+#- Carregando o Modelo
+@st.cache_resource # Cache recomendado pela Streamlit
+def carregar_modelo():
+    return joblib.load('model/modelo_final.joblib')
+
+modelo = carregar_modelo()
+
+#- Entradas
+st.header("Dados da Transação")
+
+col1, col2 = st.columns(2)
+with col1:
+    time_val = st.number_input("Tempo desde a primeira transação (Time)", min_value=0.0, value=0.0)
+with col2:
+    amount_val = st.number_input("Valor da Transação (Amount em $)", min_value=0.0, value=150.0)
+
+st.subheader("Variáveis Vx 1~28")
+st.markdown("(Não obrigatórias, mas quanto mais preenchidas, mais precisa será a predição)")
+
+#- Gerando os 28 campos automaticamente em 4 colunas para o layout não ficar feio
+with st.expander("Clique para inserir as variáveis VXs"):
+    cols = st.columns(4)
+    v_inputs = {}
+    for i in range(1, 29):
+        col_idx = (i - 1) % 4
+        with cols[col_idx]:
+            v_inputs[f'V{i}'] = st.number_input(f'V{i}', value=0.0, format="%.4f")
+
+#- Executar Predição
+st.markdown("---")
+if st.button("Executar Predição"):
+    #- Organiza os dados
+    dados_entrada = {'Time': time_val}
+    dados_entrada.update(v_inputs)
+    dados_entrada['Amount'] = amount_val
+    
+    df_input = pd.DataFrame([dados_entrada])
+    
+    #- Faz a predição 
+    proba_fraude = modelo.predict_proba(df_input)[0][1]
+    
+    #- Seta o threshold do colab, para classificar como fraude ou não 
+    threshold = 0.85
+    
+    # 5. Exibição dos Resultados
+    st.header("Resultado Finais:")
+    if proba_fraude >= threshold:
+        st.error(f"! Comportamento anormal, probabilidade de fraude: {proba_fraude:.2%})")
+        st.write("(Comportamento fora do comum, recomendado bloquear preventivamente e contatar o cliente)")
+    else:
+        st.success(f"* Comportamento normal, probabilidade de fraude: {proba_fraude:.2%})")
+        st.write("(Comportamento padrão, recomendado aprovar a transação)")
