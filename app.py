2466f853a95bba3afc40d7eb105e857b50fef748
stevalves <ealves1710@hotmail.com>
2026-06-04 03:32:01 -0300

    fix: tratativas contra erro do scikitlearn

--- app.py
+++ app.py
@@ -21,18 +21,6 @@ st.header("Dados da Transação")
 
 amount_val = st.number_input("Valor da Transação (Amount em $)", min_value=0.0, value=150.0)
 
-st.subheader("Variáveis Vx 1~28")
-st.markdown("(Não obrigatórias, mas quanto mais preenchidas, mais precisa será a predição)")
-
-#- Gerando os 28 campos automaticamente em 4 colunas para o layout não ficar feio
-with st.expander("Clique para inserir as variáveis VXs"):
-    cols = st.columns(4)
-    v_inputs = {}
-    for i in range(1, 29):
-        col_idx = (i - 1) % 4
-        with cols[col_idx]:
-            v_inputs[f'V{i}'] = st.number_input(f'V{i}', value=0.0, format="%.4f")
-
 #- Para apresentação, preferimos fazer um preenchimento automatico
 st.markdown("Teste rapido:")
 col_btn1, col_btn2, col_btn3 = st.columns(3)
@@ -56,18 +44,20 @@ with col_btn3:
             st.session_state[f'v{i}_input'] = 0.0
 
 #- Gerando os 28 campos
+st.subheader("Variáveis Vx 1~28")
+st.markdown("(Não obrigatórias, mas quanto mais preenchidas, mais precisa será a predição)")
 with st.expander("Clique para visualizar ou editar as variáveis VXs"):
     cols = st.columns(4)
     v_inputs = {}
     for i in range(1, 29):
         col_idx = (i - 1) % 4
         
-        # Se a variável ainda não existe na memória, cria ela valendo 0.0
+        #- Se a variável ainda não existe na memória, cria ela valendo 0.0
         if f'v{i}_input' not in st.session_state:
             st.session_state[f'v{i}_input'] = 0.0
             
         with cols[col_idx]:
-            # O parâmetro 'key' é o que conecta o campo visual à memória do Streamlit
+            #- O parâmetro 'key' é o que conecta o campo visual à memória do Streamlit
             v_inputs[f'V{i}'] = st.number_input(
                 f'V{i}', 
                 format="%.4f", 
